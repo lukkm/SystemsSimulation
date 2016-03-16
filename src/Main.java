f1167dfad4352e3fc6e02ac1aac88fbb3bfeef8b
Lucas Moscovicz <lmoscovicz@ElLukoHotspot.local>
2016-03-16 10:53:10 -0300

    Added brute force algorithm

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,46 @@
 import file.FileLoader;
+import model.Particle;
 import model.SimulationBoard;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length < 3) {
-            System.out.println("Input: <static file> <dynamic file> M");
+        if (args.length < 4) {
+            System.out.println("Input: <static file> <dynamic file> M rC");
             return;
         }
 
         String staticFile = args[0];
         String dynamicFile = args[1];
         int M = Integer.valueOf(args[2]);
+        float rC = Float.valueOf(args[3]);
 
         try {
             SimulationBoard board = FileLoader.loadFiles(staticFile, dynamicFile);
+            if (board == null) {
+                System.out.println("Invalid file format");
+                return;
+            }
+
+            Map<Particle, Set<Particle>> closeParticles = board.calculateBruteForceDistance(rC);
+            output(closeParticles);
         } catch (IOException e) {
             // Do something
+        }
+    }
+
+    private static void output(Map<Particle, Set<Particle>> particleSetMap) {
+        for (Particle p : particleSetMap.keySet()) {
+            System.out.print(p.getId());
+            System.out.print(": ");
+            for (Particle p2 : particleSetMap.get(p)) {
+                System.out.print(p2.getId() + " ");
+            }
+            System.out.println();
         }
     }
 
--- src/model/SimulationBoard.java
+++ src/model/SimulationBoard.java
@@ -1,6 +1,8 @@
 package model;
 
-import java.util.List;
+import utils.DistanceUtils;
+
+import java.util.*;
 
 public class SimulationBoard {
 
@@ -12,4 +14,22 @@ public class SimulationBoard {
         this.particleList = particleList;
     }
 
+    public Map<Particle, Set<Particle>> calculateBruteForceDistance(float rc) {
+        Map<Particle, Set<Particle>> closeParticles = new LinkedHashMap<>();
+        for (Particle p1 : particleList) {
+            closeParticles.put(p1, new HashSet<>());
+            for (Particle p2 : particleList) {
+                if (p1 != p2) {
+                    if (isNeighbor(p1, p2, closeParticles) || DistanceUtils.calculateDistance(p1, p2) < rc)
+                        closeParticles.get(p1).add(p2);
+                }
+            }
+        }
+        return closeParticles;
+    }
+
+    private boolean isNeighbor(Particle p1, Particle p2, Map<Particle, Set<Particle>> particleSetMap) {
+        return (particleSetMap.containsKey(p1) && particleSetMap.get(p1).contains(p2))
+                || (particleSetMap.containsKey(p2) && particleSetMap.get(p2).contains(p1));
+    }
 }
