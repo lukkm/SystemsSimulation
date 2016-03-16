123440f9bb5261ab084155dc6402ed265ca922d3
Lucas Moscovicz <lmoscovicz@ElLukoHotspot.local>
2016-03-16 10:09:48 -0300

    Added main validations

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,17 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
+        if (args.length < 3) {
+            System.out.println("Input: <static file> <dynamic file> M");
+            return;
+        }
+
+        String staticFile = args[0];
+        String dynamicFile = args[1];
+        int M = Integer.valueOf(args[2]);
+
         try {
-            SimulationBoard board = FileLoader.loadFiles(args[0], args[1]);
-            int a = 0;
+            SimulationBoard board = FileLoader.loadFiles(staticFile, dynamicFile);
         } catch (IOException e) {
             // Do something
         }
