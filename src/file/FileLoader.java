c8814ea90459a8e7868e97a779b8cdfa77ea56df
Lucas Moscovicz <lmoscovicz@ElLukoHotspot.local>
2016-03-16 09:32:03 -0300

    Initial commit

---
+++ src/file/FileLoader.java
@@ -0,0 +1,15 @@
+package file;
+
+import model.SimulationBoard;
+
+public class FileLoader {
+
+    public static SimulationBoard loadStaticFile() {
+        return null;
+    }
+
+    public static SimulationBoard loadDynamicFile() {
+        return null;
+    }
+
+}
---
+++ src/model/SimulationBoard.java
@@ -0,0 +1,11 @@
+package model;
+
+public class SimulationBoard {
+
+    private Particle[][] board;
+
+    public SimulationBoard(int l, int n) {
+        board = new Particle[l][l];
+    }
+
+}
