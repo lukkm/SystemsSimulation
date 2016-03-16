c615990776f7eeaebd8d4c38d0b14123a6580301
Lucas Moscovicz <lmoscovicz@ElLukoHotspot.local>
2016-03-16 11:38:07 -0300

    Added output

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import file.FileLoader;
 import model.Particle;
 import model.SimulationBoard;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Map;
 import java.util.Set;
@@ -9,15 +10,16 @@ import java.util.Set;
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length < 4) {
-            System.out.println("Input: <static file> <dynamic file> M rC");
+        if (args.length < 5) {
+            System.out.println("Input: <static file> <dynamic file> <output file> M rC");
             return;
         }
 
         String staticFile = args[0];
         String dynamicFile = args[1];
-        int M = Integer.valueOf(args[2]);
-        float rC = Float.valueOf(args[3]);
+        String outputFile = args[2];
+        int M = Integer.valueOf(args[3]);
+        float rC = Float.valueOf(args[4]);
 
         try {
             SimulationBoard board = FileLoader.loadFiles(staticFile, dynamicFile);
@@ -38,6 +40,7 @@ public class Main {
             System.out.println(String.format("Elapsed time for brute force: %d ms", bruteForceTime2 - bruteForceTime));
             System.out.println(String.format("Elapsed time for Cell Index Method Algorithm: %d ms", time2 - time));
 
+            file.FileWriter.printToFile(outputFile, closeParticles);
         } catch (IOException e) {
             // Do something
         }
---
+++ src/file/FileWriter.java
@@ -0,0 +1,30 @@
+package file;
+
+import model.Particle;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+import java.util.Set;
+
+public class FileWriter {
+
+    public static void printToFile(String file, Map<Particle, Set<Particle>> particleSetMap) throws IOException {
+        PrintWriter writer = new PrintWriter(file, "UTF-8");
+
+        StringBuilder builder;
+        for (Particle p : particleSetMap.keySet()) {
+            builder = new StringBuilder(p.getId());
+            builder.append(", ");
+            for (Particle p2 : particleSetMap.get(p)) {
+                builder.append(p2);
+                builder.append(", ");
+            }
+            builder.deleteCharAt(builder.length() - 1);
+            writer.println(builder.toString());
+        }
+
+        writer.close();
+    }
+
+}
